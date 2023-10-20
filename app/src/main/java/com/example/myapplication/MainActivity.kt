734d8aee624c132566e635682701596ec35505df
dltpgh98 <82332337+dltpgh98@users.noreply.github.com>
2023-10-20 19:39:11 +0900

    seho branch commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //seho
         txt_id.setOnClickListener{
             val text1 = txt_id.text.toString()
         }
